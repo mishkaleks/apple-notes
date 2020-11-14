b4f9fa9c554f69cedf6ffc0255b61ecaca1eb1fe
Mikhail Korovai <korovai.personal@gmail.com>
2020-11-14 09:05:31 +0200

    Create functionality for deleting folders

--- src/components/folder-list-item/folder-list-item.js
+++ src/components/folder-list-item/folder-list-item.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 // Redux
 import { connect } from 'react-redux';
+import { onDeleteFolder } from '../../reducers/index';
 
 // Material-UI
 import { IconButton } from '@material-ui/core';
@@ -11,7 +12,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 
 class FolderListItem extends Component {
   render() {
-    const { classes, folders } = this.props;
+    const { classes, folders, onDeleteFolder } = this.props;
     
     const items = folders.map(item => {
       const { id, title } = item;
@@ -19,7 +20,6 @@ class FolderListItem extends Component {
       return (
         <li key={id} className={classes.folderListItem}>
           <input
-            onBlur={this.onGetFolderName}
             type="text"
             id={`folderName${id}`}
             className={classes.folderName}
@@ -28,11 +28,11 @@ class FolderListItem extends Component {
           />
 
           <div className={classes.wrFolderBtns}>
-            <IconButton aria-label="edit" className={classes.editFolderNameBtn}>
+            <IconButton id={`editFolderName${id}`} aria-label="edit" className={classes.editFolderNameBtn}>
               <EditIcon className={classes.editIcon} />
             </IconButton>
 
-            <IconButton aria-label="delete" className={classes.deleteFolderNameBtn}>
+            <IconButton onClick={() => onDeleteFolder(id)} id={`deleteFolderName${id}`} aria-label="delete" className={classes.deleteFolderNameBtn}>
               <DeleteIcon className={classes.deleteIcon} />
             </IconButton>
           </div>
@@ -42,7 +42,7 @@ class FolderListItem extends Component {
 
     return (
       <React.Fragment>
-        { items }
+        {items}
       </React.Fragment>
     );
   };
@@ -54,4 +54,10 @@ const mapStateToProps = ({ folders }) => {
   };
 };
 
-export default connect (mapStateToProps)(FolderListItem);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onDeleteFolder: (id) => dispatch(onDeleteFolder(id))
+  };
+};
+
+export default connect (mapStateToProps, mapDispatchToProps)(FolderListItem);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,18 +3,27 @@ const initialState = {
 };
 
 const ADD_FOLDER_TO_NOTES = 'ADD_FOLDER_TO_NOTES';
+const DELETE_FOLDER_TO_NOTES = 'DELETE_FOLDER_TO_NOTES';
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case ADD_FOLDER_TO_NOTES:
-      const id = state.folders.length;
+      const id = state.folders.length === 0 ? 0 : state.folders[state.folders.length - 1].id + 1;
       const newFolder = {
         id,
         title: `New Foolder ${id + 1}`
       };
+
+      return {
+        folders: [...state.folders, newFolder],
+        newFolderName: state.newFolderName
+      };
+    case DELETE_FOLDER_TO_NOTES:
+      const deleteId = state.folders.findIndex((item) => item.id === action.payload);
       
       return {
-        folders: [...state.folders, newFolder]  
+        folders: [...state.folders.slice(0, deleteId), ...state.folders.slice(deleteId + 1)],
+        newFolderName: state.newFolderName
       };
     default:
       return state;
@@ -29,6 +38,14 @@ const onAddFolder = () => {
   };
 };
 
+const onDeleteFolder = (id) => {
+  return {
+    type: 'DELETE_FOLDER_TO_NOTES',
+    payload: id
+  };
+};
+
 export {
-  onAddFolder
+  onAddFolder,
+  onDeleteFolder
 };
